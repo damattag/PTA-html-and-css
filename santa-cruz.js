21502b3bf3cc6a84e913974abf6ee13b16ef2ffd
titi-lima <titisau@gmail.com>
2023-11-27 16:10:41 -0300

    feat(js-ao-vivo): dark background on click

--- santa-cruz.js
+++ santa-cruz.js
@@ -3,3 +3,10 @@ const themeButton = document.getElementById("theme-button");
 themeButton.addEventListener("click", () => {
   document.body.classList.toggle("dark-theme");
 });
+const cards = document.getElementsByClassName("card");
+
+for (let i = 0; i < cards.length; i++) {
+  cards[i].addEventListener("click", () => {
+    cards[i].style.backgroundColor = "#151515";
+  });
+}
